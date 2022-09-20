4733378d9bb1a6690340797110603266e5471710
AniqKhan03 <aniqahmedkhan123@gmail.com>
2022-09-19 23:59:35 -0400

    changed result page link, removed unnecessary component

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -1,42 +1,45 @@
 import Image from "next/image";
-import { ThumbUpIcon } from '@heroicons/react/outline'
+import { ThumbUpIcon } from "@heroicons/react/outline";
 import { forwardRef } from "react";
 import Link from "next/link";
 
 const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
-  console.log(result)
-  
+
   return (
     <Link
-          href={{
-            pathname: "/search",
-            query: result, // the data
-          }} replace
-        >
-    <div ref={ref} className="p-3 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
-      <Image
-        layout="responsive"
-        src={
-          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
-          `${BASE_URL}${result.poster.path}`
-        }
-        height={1080}
-        width={1920}
-      />
-      <div className="p-2">
-        <p className="truncate max-w-md">{result.overview}</p>
-        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">{result.title || result.original_name}</h2>
-        <p className="flex items-center opacity-0 group-hover:opacity-100">
-            {result.release_date || result.first_air_date }{" "}
+      href={{
+        pathname: "/search",
+        query: result, // the data
+      }}
+      replace
+    >
+      <div
+        ref={ref}
+        className="p-3 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+      >
+        <Image
+          layout="responsive"
+          src={
+            `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+            `${BASE_URL}${result.poster.path}`
+          }
+          height={1080}
+          width={1920}
+        />
+        <div className="p-2">
+          <p className="truncate max-w-md">{result.overview}</p>
+          <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+            {result.title || result.original_name}
+          </h2>
+          <p className="flex items-center opacity-0 group-hover:opacity-100">
+            {result.release_date || result.first_air_date}{" "}
             <ThumbUpIcon className="h-5 mx-2" /> {result.vote_count}
-        </p>
+          </p>
+        </div>
       </div>
-    </div>
     </Link>
-    
-    
   );
-})
+});
 
 export default Thumbnail;
--- pages/search.jsx
+++ pages/search.jsx
@@ -1,52 +1,45 @@
-import { useRouter } from "next/router"
-import Header from '../components/Header';
-import Link from 'next/link';
-import Image from 'next/image';
-import HeaderItem from '../components/HeaderItem';
-import {
-  HomeIcon, 
-} from "@heroicons/react/outline"
+import { useRouter } from "next/router";
+import Header from "../components/Header";
+import Image from "next/image";
 
 function Search() {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
-  const router = useRouter()
-  const result = router.query
+  const router = useRouter();
+  const result = router.query;
+  console.log(result);
 
   return (
-    <div className='flex flex-col'>
+    <div className="flex flex-col">
       <Header />
-
-      <Link href="/">
-        <a className='ml-12'><HeaderItem title='HOME' Icon={HomeIcon} /></a>
-      </Link>
       <div className="flex justify-center">
-        <div className='max-w-2xl max-h-2xl'>
+        <div className="max-w-2xl max-h-2xl">
           <Image
             src={
-              `https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}` ||
-              `https://image.tmdb.org/t/p/original/${result.poster.path}`
+              `https://image.tmdb.org/t/p/original/${
+                result.backdrop_path || result.poster_path
+              }` || `https://image.tmdb.org/t/p/original/${result.poster.path}`
             }
             height={1080}
             width={1920}
             objectFit="cover"
           />
         </div>
-        
-        <div className='flex justify-center pl-8'>
-        <div className="p-2">
-          <h2 className="mt-1 text-4xl text-white p-2">{result.title || result.original_name}</h2>
-          <p className="text-xl max-w-lg p-2">{result.overview}</p>
 
-          <p className="flex items-center text-lg p-2">
-            Release Date: {result.release_date || result.first_air_date}{" "}
-          </p>
+        <div className="flex justify-center pl-8">
+          <div className="p-2">
+            <h2 className="mt-1 text-4xl text-white p-2">
+              {result.title || result.original_name}
+            </h2>
+            <p className="text-xl max-w-lg p-2">{result.overview}</p>
+
+            <p className="flex items-center text-lg p-2">
+              Release Date: {result.release_date || result.first_air_date}{" "}
+            </p>
+          </div>
         </div>
-        </div>
-        
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
